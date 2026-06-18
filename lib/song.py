d59aeee7d9d3178a381745fde7a9b95d59301444
janenyasoro <janenyasoro@gmail.com>
2026-06-18 06:56:29 +0300

    Python-based Command-Line Interface (CLI) application that manages a simulated multi-user project tracker system.

--- lib/song.py
+++ lib/song.py
@@ -1,32 +1,19 @@
 # lib/song.py
 
 class Song:
-    """
- A class to represent a song in the music library system.
-    """
-  
-    # Class attributes to track global song data
-    count = 0  # Total number of songs created
-    genres = []  # List of unique genres across all songs
-    artists = []  # List of unique artists across all songs
-    genre_count = {}  # Dictionary mapping genre -> number of songs
-    artists_count = {}  # Dictionary mapping artist -> number of songs
+    # Class attributes
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artists_count = {}
     
     def __init__(self, name, artist, genre):
-        """
-        Initialize a new Song instance.
-        
-        Args:
-            name (str): The name/title of the song
-            artist (str): The artist who performed the song
-            genre (str): The musical genre of the song
-        """
         self.name = name
         self.artist = artist
         self.genre = genre
         
-        # These methods must be called manually in tests
-        # but we'll also call them automatically for convenience
+        # Call class methods to update statistics
         Song.add_song_to_count()
         Song.add_to_genres(genre)
         Song.add_to_artists(artist)
@@ -35,34 +22,20 @@ class Song:
     
     @classmethod
     def add_song_to_count(cls):
-        """Increment the total song count by one."""
         cls.count += 1
     
     @classmethod
     def add_to_genres(cls, genre):
-        """
-        Add a new genre to the global genres list if it doesn't already exist.
-        Ensures only unique genres are stored.
-        """
         if genre not in cls.genres:
             cls.genres.append(genre)
     
     @classmethod
     def add_to_artists(cls, artist):
-        """
-        Add a new artist to the global artists list if they don't already exist.
-        Ensures only unique artists are stored.
-        """
         if artist not in cls.artists:
             cls.artists.append(artist)
     
     @classmethod
     def add_to_genre_count(cls, genre):
-        """
-    Update the genre count dictionary.
-    Increments the count for the given genre by 1. If the genre doesn't
-    exist in the dictionary, it's added with a count of 1.
-        """
         if genre in cls.genre_count:
             cls.genre_count[genre] += 1
         else:
@@ -70,12 +43,7 @@ class Song:
     
     @classmethod
     def add_to_artists_count(cls, artist):
-        """
-    Update the artist count dictionary.
-    Increments the count for the given artist by 1. If the artist doesn't
-    exist in the dictionary, they're added with a count of 1.
-        """
         if artist in cls.artists_count:
             cls.artists_count[artist] += 1
         else:
-         cls.artists_count[artist] = 1
+            cls.artists_count[artist] = 1
